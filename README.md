7b5fe6f5e5fca601fb5ca58795cedd7c5739241a
anirban-chakraborty <42816536+anirban-chakraborty@users.noreply.github.com>
2018-11-13 21:24:18 +0530

    First Update

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# CollegeWeb
+This is the student portal of the CollegeWeb Service. 
+
+Update includes:
+
+1. Timeline
+2. Messages
+3. Likes
+4. Comments
+5. About Me
+6. Notifications
+7. Status Updates
+8. Followers
+9. Audio & Video Support
+10. Verified Accounts
+11. Profile Images
+
+
+=========================================================================================================
+
+Please note, this build validates users by using cookies instead of sessions. Cookies can be found 
+marked with packets SNID & SNID_. 
+
+=========================================================================================================
+
+--------------------------------------------
+Support
+--------------------------------------------
+
+>> AJAX has been used at places such as comments for easier live updates. 
+>> Profile Prictures are implemented using IMGUR API. 
+>> All HTML designs have been completed by using BOOTSTRAP STUDIO.
+
+--------------------------------------------
+For Developers
+--------------------------------------------
+
+>> This build is in no way complete. The functionalities are implemented but tidying up still remains to be done. 
+Implementation of design on PHP is left to the lead developer to complete. Also, sequencing of pages need to be 
+done so that when clicked, it redirects to the logical next page. 
+
+>> If importing the SQL produces an error on PhpMyAdmin, uncheck 'partial import', 'enable foreign key check' and 'Do not   use AUTO_INCREMENT for zero values'. The proper way to do it is to create a new database called 'social network' and then import the tables from the file included. 
+
+
+>> Also, errors might be produced because of the user data dump. In that case, open the sql file in Sublime Text/Atom/Notepad++ and delete the user data that I compiled at the time of testing. 
+
+----------------------------------------------------------------------------------------------------------------------------
+
+The best way to predict the future is to invent it.
+													-- Alan Kay
---
+++ socialnetwork.sql
@@ -0,0 +1,328 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2018 at 04:41 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 5.6.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `socialnetwork`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `comment` text NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `posted_at` datetime NOT NULL,
+  `post_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `followers`
+--
+
+CREATE TABLE `followers` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `follower_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_tokens`
+--
+
+CREATE TABLE `login_tokens` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `token` char(64) NOT NULL DEFAULT '',
+  `user_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login_tokens`
+--
+
+INSERT INTO `login_tokens` (`id`, `token`, `user_id`) VALUES
+(1, 'dde186f0f7d873f79f1dfd9467990cc88992c7f3', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `body` text NOT NULL,
+  `sender` int(11) UNSIGNED NOT NULL,
+  `receiver` int(11) UNSIGNED NOT NULL,
+  `read` tinyint(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `type` int(11) UNSIGNED NOT NULL,
+  `receiver` int(10) UNSIGNED NOT NULL,
+  `sender` int(11) UNSIGNED NOT NULL,
+  `extra` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_tokens`
+--
+
+CREATE TABLE `password_tokens` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `token` char(64) NOT NULL DEFAULT '',
+  `user_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `body` varchar(160) NOT NULL DEFAULT '',
+  `posted_at` datetime NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `likes` int(11) UNSIGNED NOT NULL,
+  `postimg` varchar(255) DEFAULT NULL,
+  `topics` varchar(400) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `body`, `posted_at`, `user_id`, `likes`, `postimg`, `topics`) VALUES
+(1, 'Hello World!', '2018-11-13 15:41:04', 1, 0, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_likes`
+--
+
+CREATE TABLE `post_likes` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `post_id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(32) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `email` text,
+  `verified` tinyint(1) NOT NULL DEFAULT '0',
+  `profileimg` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `verified`, `profileimg`) VALUES
+(1, 'anirban', '$2y$10$UTWo09PDhrtSc0/L8Xf9cOoMRb./9JY45AXzypYbTSYrViRuLfvj2', 'prez@applicationone.in', 0, ''),
+(2, 'Debojyoti', '$2y$10$aw78/56KOU7xjsgp1FnPRun6dMJB.LstBZALVN2Vde0OLmCj5Jv1q', 'developer@applicationone.in', 0, ''),
+(3, 'sankha', '$2y$10$qfDoSBWWvJbCa3wtrYRpFOxJH4OIutQ1oQk4Q44hTQXxXWeR6.eAC', 'comms@applicationone.in', 0, ''),
+(4, 'santan', '$2y$10$xIcOhgkl17Dky0jciZKoG.mrifcVRKyqVyCBAI1HtqPII9d6bKQdi', 'santan@applicationone.in', 0, ''),
+(5, 'devshreshth', '$2y$10$CXagxYpmbwCxIlKyxRAOQeSJC2INMIjoX0rk/KSBgvoGqkaiFszcC', 'evp@applicationone.in', 0, ''),
+(6, 'verified', '$2y$10$obzaCyWDSL97PnMWO5xt4uIsxTNqpq8vx1kFLh1P0WEtwHy4B23jy', 'verified@applicationone.in', 0, '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `post_id` (`post_id`);
+
+--
+-- Indexes for table `followers`
+--
+ALTER TABLE `followers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_tokens`
+--
+ALTER TABLE `login_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_tokens`
+--
+ALTER TABLE `password_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `post_likes`
+--
+ALTER TABLE `post_likes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `post_id` (`post_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `followers`
+--
+ALTER TABLE `followers`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `login_tokens`
+--
+ALTER TABLE `login_tokens`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `password_tokens`
+--
+ALTER TABLE `password_tokens`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `post_likes`
+--
+ALTER TABLE `post_likes`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
+
+--
+-- Constraints for table `login_tokens`
+--
+ALTER TABLE `login_tokens`
+  ADD CONSTRAINT `login_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `post_likes`
+--
+ALTER TABLE `post_likes`
+  ADD CONSTRAINT `post_likes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
